fb23a777b3da29384c11418731a96af1e812b79e
lud-ic <mag.ludmila@gmail.com>
2022-04-06 19:03:28 -0300

    feat: WIP adjusting cards
    Co-authored-by: JackeAguiar <jackeaguiar000@gmail.com>
    Co-authored-by: WayraArendartchukCastro <wayraac@gmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,36 +1,42 @@
-import data from './data/pokemon/pokemon.js';
-import { searchByName, selectType, calcType, sortAZ, } from './data.js'
-
+import data from "./data/pokemon/pokemon.js";
+import { searchByName, selectType, calcType, sortAZ } from "./data.js";
 
 const pokemons = data.pokemon;
 
 function cardsPokemons(data) {
   document.getElementById("calculation").innerHTML = "";
   const cardPokemon = document.getElementById("cards");
-  cardPokemon.innerHTML = data.map((item) =>
-    `<div class="info-cards" id="flip"> 
-   <p id="num">  ${item.num}</p> 
-   <img class="img-card" src="${item.img}"/> 
-   <p class='title'>Name: </p>
-   <p id="name">${item.name}</p> 
-   <p class='title'>Type:</p>
-   <p id="type" >${item.type.join(' | ')}</p> 
-   <p class='title'>About:</p>
-   <p id="about">${item.about}</p> 
-   </div>`
-
-  ).join("")
-
+  cardPokemon.innerHTML = data
+    .map(
+      (item) => `
+    <div class="info-cards">
+      <div class="img-name">
+        <img class="img-card" src="${item.img}"/>
+        <p class="name"> ${item.name}</p>
+      </div>
+      <div class="card-details">
+        <p class="title"> <strong> Rarity: </strong> ${item["pokemon-rarity"]}</p>
+        <p class="title"><strong> Generation: </strong> ${item.generation.name}</p>
+        <p class="title"><strong> Type: </strong> ${item.type.join(" | ")}</p>
+        <p class="title"><strong> Weaknesses: </strong> ${item.weaknesses.join(" | ")}</p>
+        <p class="title"><strong> Resistant: </strong> ${item.resistant.join(" | ")}</p>
+      </div>
+      <div class="number-container">
+        <p class="num">  ${item.num}</p>
+      </div>
+    </div>`
+    )
+    .join("");
 }
 cardsPokemons(pokemons);
 
-const search = document.getElementById("inputSearch")
+const search = document.getElementById("inputSearch");
 
 search.addEventListener("keypress", (e) => {
   const searchName = e.search.value;
   const pokemonsName = searchByName(searchName, pokemons);
   cardsPokemons(pokemonsName);
-})
+});
 
 const filterType = document.querySelector(".select-typefilters");
 
@@ -39,17 +45,15 @@ filterType.addEventListener("change", () => {
   const arrFilter = selectType(filterbyType, pokemons);
   cardsPokemons(arrFilter);
   typePercent();
-})
-
+});
 
 function typePercent() {
-
   const filterType = document.querySelector(".select-typefilters").value;
   let result = calcType(pokemons, filterType);
-  document.getElementById("calculation").innerText += `The selected pokémon represent ${result}% of the total.`
+  document.getElementById(
+    "calculation"
+  ).innerText += `The selected pokémon represent ${result}% of the total.`;
 }
-
-
 
 const sortOrder = document.getElementById("sortAlphabet");
 
@@ -57,10 +61,8 @@ sortOrder.addEventListener("change", (event) => {
   const selectedSort = event.target.value;
   const filterAz = sortAZ(pokemons, selectedSort);
   cardsPokemons(filterAz);
+});
 
-})
-
-const refreshPage = document.getElementById("clearButton")
+const refreshPage = document.getElementById("clearButton");
 
 refreshPage.addEventListener("click", () => cardsPokemons(pokemons));
-
