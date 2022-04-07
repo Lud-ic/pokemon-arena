21045c70c5982d5dc44a0925ad415fd0b1ddb62d
lud-ic <mag.ludmila@gmail.com>
2022-04-07 17:17:59 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,30 @@
+export const searchByName = (selectByName, pokemons) =>
+    pokemons.filter((select) => select.name.includes(selectByName));
+
+export const selectType = (selectByType, pokemons) =>
+    pokemons.filter((select) => select.type.includes(selectByType));
+
+export const selectResistant = (selectByResistant, pokemons) =>
+    pokemons.filter((select) => select.resistant.includes(selectByResistant));
+
+export const selectWeaknesses = (selectByWeaknesses, pokemons) =>
+    pokemons.filter((select) => select.weaknesses.includes(selectByWeaknesses));
+
+export const selectRarity = (selectByRarity, pokemons) => {
+    const filteredRarity = selectByRarity.filter(pokemonRarity => pokemonRarity ["pokemon-rarity"] == pokemons)
+    return filteredRarity
+}
+
+export const percentageCalc = (total,portion) => {
+    const percentage = Math.round((portion * 100)/total).toFixed(2);
+    return percentage
+}
+
+export const sortAZ = (data, order) => {
+    const sortaz = [...data]
+    if (order === "A-Z") {
+        return sortaz.sort((a, z) => a.name > z.name ? 1 : -1);
+    } else {
+        return sortaz.sort((a, z) => a.name > z.name ? -1 : 1);
+    }
+}
---
+++ main.js
@@ -0,0 +1,109 @@
+import data from './data/pokemon/pokemon.js';
+import { searchByName, selectType, percentageCalc,selectRarity, sortAZ, selectWeaknesses, selectResistant } from './data.js'
+
+const pokemons = data.pokemon;
+
+function cardsPokemons(data) {
+  document.getElementById("calculation").innerHTML = "HERE ARE ALL POKÉMONS";
+  const cardPokemon = document.getElementById("cards");
+  cardPokemon.innerHTML = data
+    .map(
+      (item) => `
+    <div class="info-cards">
+      <div class="img-and-name-container">
+        <img class="img-card" src="${item.img}"/>
+        <p class="name"> ${item.name}</p>
+      </div>
+      <div class="card-details">
+        <p><strong> Rarity: </strong> ${item["pokemon-rarity"]}</p>
+        <p><strong> Generation: </strong> ${item.generation.name}</p>
+        <p><strong> Type: </strong> ${item.type.join(" | ")}</p>
+        <p><strong> Weaknesses: </strong> ${item.weaknesses.join(" | ")}</p>
+        <p><strong> Resistant: </strong> ${item.resistant.join(" | ")}</p>
+      </div>
+      <div class="number-container">
+        <p class="num">  ${item.num}</p>
+      </div>
+    </div>`
+    )
+    .join("");
+}
+cardsPokemons(pokemons);
+
+const filterType = document.querySelector("#types-filter");
+const sortOrder = document.querySelector("#sortAlphabet");
+const filterRarity = document.querySelector("#rarity-filter")
+const filterWeaknesses = document.querySelector("#weaknesses-filter")
+const filterResistant = document.querySelector("#resistant-filter")
+
+
+const refreshPage = document.getElementById("clearButton")
+const search = document.getElementById("inputSearch")
+const percentage = document.getElementById("calculation")
+
+
+function showPercentage(textPercentage){
+  percentage.innerHTML = `THERE ARE ${textPercentage}`
+}
+
+filterType.addEventListener("change", (e) => {
+  e.preventDefault;
+  const arrFilter = selectType(filterType.value, pokemons);
+  filterRarity.selectedIndex=0;
+  filterWeaknesses.selectedIndex=0;
+  filterResistant.selectedIndex=0;
+  cardsPokemons(arrFilter);
+  const typePercentage = `${percentageCalc(pokemons.length,arrFilter.length)}% POKÉMONS FROM THIS CATEGORY`
+  showPercentage(typePercentage);
+})
+
+filterResistant.addEventListener("change", (e) => {
+  e.preventDefault;
+  const arrResistant = selectResistant(filterResistant.value, pokemons);
+  filterRarity.selectedIndex=0;
+  filterWeaknesses.selectedIndex=0;
+  filterType.selectedIndex=0;
+  cardsPokemons(arrResistant);
+  const resistantPercentage = `${percentageCalc(pokemons.length,arrResistant.length)}% POKÉMONS FROM THIS CATEGORY`
+  showPercentage(resistantPercentage);
+})
+
+filterWeaknesses.addEventListener("change", (e) => {
+  e.preventDefault;
+  const arrWeaknesses = selectWeaknesses(filterWeaknesses.value, pokemons);
+  filterRarity.selectedIndex=0;
+  filterResistant.selectedIndex=0;
+  filterType.selectedIndex=0;
+  cardsPokemons(arrWeaknesses);
+  const WeaknessesPercentage = `${percentageCalc(pokemons.length,arrWeaknesses.length)}% POKÉMONS FROM THIS CATEGORY`
+  showPercentage(WeaknessesPercentage);
+})
+
+filterRarity.addEventListener("change", (e) => {
+  e.preventDefault;
+  const arrRarity = selectRarity(pokemons, e.target.value)
+  filterWeaknesses.selectedIndex=0;
+  filterResistant.selectedIndex=0;
+  filterType.selectedIndex=0;
+  cardsPokemons(arrRarity)
+  const typePercentage = `${percentageCalc(pokemons.length,arrRarity.length)}% POKÉMONS FROM THIS CATEGORY`
+  showPercentage(typePercentage);
+})
+
+
+sortOrder.addEventListener("change", (e) => {
+  e.preventDefault;
+  const selectedSort = e.target.value;
+  const filterAz = sortAZ(pokemons, selectedSort);
+  cardsPokemons(filterAz);
+})
+
+search.addEventListener("keypress", (e) => {
+  e.preventDefault;
+  const pokemonsName = searchByName(search.value, pokemons);
+  cardsPokemons(pokemonsName);
+  const namePercentage = `${percentageCalc(pokemons.length,pokemonsName.length)}% FROM THIS CATEGORY`
+  showPercentage(namePercentage);
+})
+
+refreshPage.addEventListener("click", () => cardsPokemons(pokemons));
